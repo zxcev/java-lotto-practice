1241bd9be5fb69f966ec63be8284d2a48b4de40d
zxcev <121966058+zxcev@users.noreply.github.com>
2023-11-23 15:25:30 +0900

    fix: 로또 생성 시 사용되는 라이브러리 변경

--- src/main/java/lotto/domain/lotto/LottoGenerator.java
+++ src/main/java/lotto/domain/lotto/LottoGenerator.java
@@ -1,29 +1,19 @@
 package lotto.domain.lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
 public final class LottoGenerator {
 
     public static Lotto generate() {
-        final Set<Integer> uniqueNumbers = new HashSet<>();
-
-        while (uniqueNumbers.size() < Lotto.NUMBERS_LENGTH) {
-            final int number = generateNumber();
-            uniqueNumbers.add(number);
-        }
-
-        return Lotto.from(
-                new ArrayList<>(uniqueNumbers)
-        );
+        return Lotto.from(generateNumber());
     }
 
-    private static int generateNumber() {
-        return Randoms.pickNumberInRange(
+    private static List<Integer> generateNumber() {
+        return Randoms.pickUniqueNumbersInRange(
                 Lotto.MIN_NUMBER,
-                Lotto.MAX_NUMBER
+                Lotto.MAX_NUMBER,
+                Lotto.NUMBERS_LENGTH
         );
 
     }
