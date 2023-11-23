7fc4bd36d88377905ae04dda74e6cd7f584a580b
zxcev <121966058+zxcev@users.noreply.github.com>
2023-11-23 13:22:45 +0900

    feat: 구매한 로또 및 당첨 통계 출력(미구현)

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,16 @@
+package lotto.view;
+
+import lotto.controller.dto.LottosDto;
+
+public final class OutputView {
+
+    public static void printPurchasedLottos(LottosDto lottosDto) {
+        System.out.println("\n8개를 구매했습니다.");
+        // TODO
+    }
+
+    public static void printDrawingResult() {
+        // TODO
+        System.out.println("\n당첨 통계\n---\n");
+    }
+}
