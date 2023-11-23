ccfe9632435a394fbb4c44353af8b6fa71d25465
zxcev <121966058+zxcev@users.noreply.github.com>
2023-11-23 13:57:57 +0900

    feat: 임의의 로또 생성 기능

---
+++ src/main/java/lotto/domain/lotto/LottoGenerator.java
@@ -0,0 +1,30 @@
+package lotto.domain.lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public final class LottoGenerator {
+
+    private static Lotto generate() {
+        final Set<Integer> uniqueNumbers = new HashSet<>();
+
+        while (uniqueNumbers.size() < Lotto.NUMBERS_LENGTH) {
+            final int number = generateNumber();
+            uniqueNumbers.add(number);
+        }
+
+        return Lotto.from(
+                new ArrayList<>(uniqueNumbers)
+        );
+    }
+
+    private static int generateNumber() {
+        return Randoms.pickNumberInRange(
+                Lotto.MIN_NUMBER,
+                Lotto.MAX_NUMBER
+        );
+
+    }
+}
