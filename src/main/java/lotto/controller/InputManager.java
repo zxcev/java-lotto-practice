649d0d4ca11cff0ae92dcea5c3abe751c1e2c649
zxcev <121966058+zxcev@users.noreply.github.com>
2023-11-23 15:24:54 +0900

    feat: 예외 발생 시 재시작 기능 추가

--- src/main/java/lotto/controller/InputManager.java
+++ src/main/java/lotto/controller/InputManager.java
@@ -2,33 +2,44 @@ package lotto.controller;
 
 import java.util.Arrays;
 import java.util.List;
+import lotto.common.RetryHandler;
 import lotto.domain.WinningLotto;
 import lotto.view.InputView;
 
 public final class InputManager {
 
+
     public static int inputPurchaseAmount() {
+        return RetryHandler.handleRetry(InputManager::_inputPurchaseAmount);
+    }
+
+    public static WinningLotto inputWinningLotto() {
+        return WinningLotto.from(
+                RetryHandler.handleRetry(InputManager::_inputWinningNumbers),
+                RetryHandler.handleRetry(InputManager::_inputBonusNumber)
+        );
+    }
+
+    private static int _inputPurchaseAmount() {
         final String input = InputView.inputPurchaseAmount();
-        InputValidator.validateNumericInput(input);
+        InputValidator.validatePurchaseAmountInput(input);
 
         return Integer.parseInt(input);
     }
 
-    public static WinningLotto inputWinningLotto() {
-        final String winningNumbersInput = InputView.inputWinningNumbers();
-        final String bonusNumberInput = InputView.inputBonusNumber();
-        InputValidator.validateWinningNumbersInput(winningNumbersInput);
-        InputValidator.validateNumericInput(bonusNumberInput);
+    private static List<Integer> _inputWinningNumbers() {
+        final String input = InputView.inputWinningNumbers();
+        InputValidator.validateWinningNumbersInput(input);
 
-        return WinningLotto.from(
-                toChunks(winningNumbersInput),
-                Integer.parseInt(bonusNumberInput)
-        );
-    }
-
-    private static List<Integer> toChunks(final String lottoNumbers) {
-        return Arrays.stream(lottoNumbers.split(","))
-                .map((Integer::parseInt))
+        return Arrays.stream(input.split(","))
+                .map(Integer::parseInt)
                 .toList();
     }
+
+    private static int _inputBonusNumber() {
+        final String bonusNumberInput = InputView.inputBonusNumber();
+        InputValidator.validateNumericInput(bonusNumberInput);
+        return Integer.parseInt(bonusNumberInput);
+    }
+
 }
